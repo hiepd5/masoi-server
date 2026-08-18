7807fb0ed06f4f224d75c8cc108305d20f05731f
hiepd5 <jeamin2811@gmail.com>
2026-08-18 21:51:53 +0700

    add masoi.hiepd5.com to CORS

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: ["https://masoi-client.vercel.app", "https://hiepd5.com", "http://localhost:5173"] },
+  cors: { origin: ["https://masoi-client.vercel.app", "https://masoi.hiepd5.com", "https://hiepd5.com", "http://localhost:5173"] },
 });
 
 function broadcastRoom(room) {
